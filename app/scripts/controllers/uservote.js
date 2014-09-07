25a660290fe6d82b056f3d1af8831b6d080d59e8
Farah Hani Fauzudin <fhani@umich.edu>
2014-09-07 00:27:47 -0400

    Added visual button toggle to the voting page using ng-class

--- app/scripts/controllers/uservote.js
+++ app/scripts/controllers/uservote.js
@@ -11,15 +11,19 @@
  .controller('UservoteCtrl', function($scope, $routeParams, $location, voteService) {
   var voteSession = voteService($routeParams.sessionId);
 
-
   voteSession.sessionExists().then(function(exists) {
     if (!exists) {
       $location.path('/');
       return;
     }
+
     voteSession.onVoteResultsChanged(function() {
       $scope.voteNode = voteSession.getChoices();
-      $scope.onVote = voteSession.addVote;
+      $scope.onVote = function(choice) {
+        voteSession.addVote(choice);
+        $scope.curChoice = choice;
+      };
+
     });
   });
 
