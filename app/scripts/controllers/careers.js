19b1686a8bee2efda8967702c8e6aa3e45975d8c
Mohammad Suffian Hamzah <suffianhamzah@gmail.com>
2014-09-07 05:48:10 -0400

    really uploaded pics

---
+++ app/scripts/controllers/careers.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name voteappApp.controller:CareersCtrl
+ * @description
+ * # CareersCtrl
+ * Controller of the voteappApp
+ */
+angular.module('voteappApp')
+  .controller('CareersCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
