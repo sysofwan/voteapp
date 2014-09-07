64e9b063bd2f6dcfd9723d07b40350bb3501048c
Sayyid Sofwan <sysofwan@umich.edu>
2014-09-07 03:33:15 -0400

    added several features to delete, reset, pause, etc

--- app/scripts/controllers/votesummary.js
+++ app/scripts/controllers/votesummary.js
@@ -8,87 +8,94 @@
  * Controller of the voteappApp
  */
 
-angular.module('voteappApp')
-  .controller('VotesummaryCtrl', function($scope, $routeParams, voteService, _, $interval) {
+ angular.module('voteappApp')
+ .controller('VotesummaryCtrl', function($scope, $routeParams, voteService, _, $interval, $location) {
 
-    var ticks,
-      voteSession = voteService($routeParams.sessionId);
+  var ticks,
+  voteSession = voteService($routeParams.sessionId);
 
-    $scope.timer = '-:--';
+  $scope.timer = '-:--';
 
-    voteSession.sessionExists().then(function(exists) {
-      if (!exists) {
-        voteSession.createSession();
-      }
-      voteSession.onGraphDataChanged(function(results) {
+  voteSession.sessionExists().then(function(exists) {
+    if (!exists) {
+      voteSession.createSession();
+    }
+    voteSession.onGraphDataChanged(function(results) {
 
-        $scope.nodeData = voteSession.getSessionSummary();
-        $scope.myData = _.map(results, function(arr, index) {
-          return {
-            label: arr[0],
-            data: [
-              [index + 1, arr[1]]
-            ]
-          };
-        });
-
-        ticks = _.map(results, function(arr, index) {
-          return [index + 1, arr[0]];
-        });
-
-        $scope.myChartOptions = {
-        	yaxis: {
-        		min: 0,
-        		tickDecimals: 0,
-            tickLength: 0
-        	},
-          xaxis: {
-            ticks: ticks,
-            min: 0.3,
-            tickLength:0,
-            max:4.7,
-            tickSize:1.5
-          },
-          bars: {
-            show: true,
-            align: 'center',
-            barWidth: 0.3
-          },
-          grid: {
-            hoverable: true
-          },
-          tooltip: true,
-          tooltipOpts: {
-            content: '%y'
-          },
-          legend: {
-            show: true,
-            noColumns: 4,
-            position: 'nw',
-            margin: 20,
-            labelFormatter: function labelFormatter(label) {
-    return "<div style='font-size:15pt;'>" + label + "</div>";
-            }
-          }
+      $scope.nodeData = voteSession.getSessionSummary();
+      $scope.myData = _.map(results, function(arr, index) {
+        return {
+          label: arr[0],
+          data: [
+          [index + 1, arr[1]]
+          ]
         };
-
-
-        $scope.voteCount = _.reduce(results, function(memo, arr) {
-          return memo + arr[1];
-        }, 0);
-
-        $scope.stopSession = voteSession.stopSession;
-        $scope.sessionStopped = voteService.sessionStopped;
-
       });
 
-      $interval(function() {
-        var timer = voteSession.getSecondsLive();
-        var sec = timer % 60;
-        var secStr = sec < 10 ? '0' + sec : sec;
-        var minute = Math.floor(timer/60);
-        $scope.timer = minute + ':' + secStr;
-      }, 1000);
-    });
+      ticks = _.map(results, function(arr, index) {
+        return [index + 1, arr[0]];
+      });
+
+      $scope.myChartOptions = {
+       yaxis: {
+        min: 0,
+        tickDecimals: 0,
+        tickLength: 0
+      },
+      xaxis: {
+        ticks: ticks,
+        min: 0.3,
+        tickLength:0,
+        tickSize:1.5,
+        max: ticks.length+1
+      },
+      bars: {
+        show: true,
+        align: 'center',
+        barWidth: 0.3
+      },
+      grid: {
+        hoverable: true
+      },
+      tooltip: true,
+      tooltipOpts: {
+        content: '%y'
+      },
+      legend: {
+        show: true,
+        noColumns: ticks.length,
+        position: 'nw',
+        margin: 20,
+        labelFormatter: function labelFormatter(label) {
+          return '<div style=\'font-size:15pt;\'>' + label + '</div>';
+        }
+      }
+    };
+
+
+    $scope.voteCount = _.reduce(results, function(memo, arr) {
+      return memo + arr[1];
+    }, 0);
+
+    $scope.pauseSession = voteSession.pauseSession;
+    $scope.resumeSession = voteSession.resumeSession;
+    $scope.resetSession = voteSession.resetSession;
+    $scope.deleteSession = function() {
+      voteSession.deleteSession();
+      $location.path('/');
+    };
+
+    $scope.sessionPaused = voteSession.sessionPaused();
 
   });
+
+$interval(function() {
+  var timer = voteSession.getSecondsLive();
+  var sec = timer % 60;
+  var secStr = sec < 10 ? '0' + sec : sec;
+  var minute = Math.floor(timer/60);
+  $scope.timer = minute + ':' + secStr;
+}, 1000);
+});
+
+});
--- app/scripts/services/voteservice.js
+++ app/scripts/services/voteservice.js
@@ -74,7 +74,7 @@ angular.module('voteappApp')
       });
 
       var addVote = function(vote) {
-        if (nodeData.info.stopped) {
+        if (nodeData.info.paused) {
           return;
         }
 
@@ -83,27 +83,29 @@ angular.module('voteappApp')
         sessionNode.child('votes').update(obj);
       };
 
-      var stopSession = function() {
-        if (!nodeData.info.stopped) {
-          nodeData.info.stopped = true;
-          nodeData.info.stoppedTime = Firebase.ServerValue.TIMESTAMP;
+      var pauseSession = function() {
+        if (!nodeData.info.paused) {
+          nodeData.info.paused = true;
+          nodeData.info.pausedTime = new Date().getTime();
           nodeData.$save();
         }
       };
 
-      var sessionStopped = function() {
-        return nodeData.info.stopped;
+      var sessionPaused = function() {
+        return nodeData.info.paused;
       };
 
       var getSecondsLive = function() {
         var created = nodeData.info.timestamp,
             time;
-        if (sessionStopped()) {
-          time = nodeData.info.stoppedTime;
+        if (sessionPaused()) {
+          time = nodeData.info.pausedTime;
         } else {
           time = new Date().getTime();
         }
-        return Math.floor((time - created) / 1000);
+
+        var millsSinceCreated = time - created < 0 ? 0 : time - created;
+        return Math.floor(millsSinceCreated/1000);
       };
 
       var getChoices = function() {
@@ -111,6 +113,25 @@ angular.module('voteappApp')
           return nodeData.info.choices;
         }
         return [];
+      };
+
+      var resumeSession = function() {
+        if (nodeData.info.paused) {
+          nodeData.info.paused = false;
+          nodeData.info.timestamp += new Date().getTime() - nodeData.info.pausedTime;
+          nodeData.$save();
+        }
+      };
+
+      var resetSession = function() {
+        nodeData.votes = 0;
+        nodeData.info.timestamp = 0;
+        nodeData.info.pausedTime = 0;
+        nodeData.$save();
+      };
+
+      var deleteSession = function() {
+        sync.$remove();
       };
 
       return {
@@ -124,10 +145,13 @@ angular.module('voteappApp')
           graphDataChangedCallback = callback;
         },
         addVote: addVote,
-        stopSession: stopSession,
+        pauseSession: pauseSession,
         getSecondsLive: getSecondsLive,
-        sessionStopped: sessionStopped,
-        getChoices: getChoices
+        sessionPaused: sessionPaused,
+        getChoices: getChoices,
+        resumeSession: resumeSession,
+        resetSession: resetSession,
+        deleteSession: deleteSession
       };
     };
   });
