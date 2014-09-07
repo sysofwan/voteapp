e464781d9575ee607b0cfcb399b9488e96904e0e
Sayyid Sofwan <sysofwan@umich.edu>
2014-09-07 05:32:07 -0400

    deleted

---
+++ phoneback/env/lib/python2.7/site-packages/twilio/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import sys
+
+from six import u
+
+# Backwards compatibility.
+from .version import __version__, __version_info__
--- phoneback/firebase.py
+++
@@ -1,5 +0,0 @@
-from firebase import firebase
-
-def send_to_firebase(userId, sessionId, vote):
-	pass
-	
